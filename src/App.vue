3d65c408be5719fecbbaa74454d036c1ad90fa94
krlmari <krylova_m_v@inbox.ru>
2024-02-24 19:55:00 +0100

    add vue-todos project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Hello" />
+    </div>
+  </header>
+
+  <main></main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+</style>
